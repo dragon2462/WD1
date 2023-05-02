e0ee4556ac8ef6c91e2052b6cd12341e1f702a91
dragon2462 <powerzheng21205@gmail.com>
2023-05-02 10:52:30 -0400

    Zeta4

--- script.js
+++ script.js
@@ -1,2 +1,4 @@
 document.getElementById("p").innerHTML = "New text!";
 document.getElementById("title").innerHTML = "Welcome to my portfolio!";
+document.querySelector('link[rel="shortcut icon"]').href = "new-favicon.ico";
+
