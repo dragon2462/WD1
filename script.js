28d85012b2913bc387a6b1e48534ccadddedd21f
dragon2462 <powerzheng21205@gmail.com>
2023-05-02 00:37:12 -0400

    Beta3

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
-const menuIcon = document.querySelector('.menu-icon');
-const menu = document.querySelector('.menu');
 
-menuIcon.addEventListener('click', () => {
-  menuIcon.classList.toggle('show-menu');
-  menu.classList.toggle('show-menu');
-});
