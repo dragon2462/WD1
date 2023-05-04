8384880ba1b1efe44c55392ee531a950564eb835
dragon2462 <121273332+dragon2462@users.noreply.github.com>
2023-05-03 21:21:57 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
-document.getElementById("p").innerHTML = "New text!";
-document.getElementById("title").innerHTML = "Welcome to my portfolio!";
-document.querySelector('link[rel="shortcut icon"]').href = "new-favicon.ico";
+// Add or remove class to show or hide the hamburger menu based on the viewport width
+function toggleHamburgerMenu() {
+  var hamburger = document.querySelector('.hamburger');
+  var navBar = document.querySelector('.nav-bar');
+
+  if (window.innerWidth <= 768) {
+    hamburger.classList.add('active');
+    navBar.classList.add('active');
+  } else {
+    hamburger.classList.remove('active');
+    navBar.classList.remove('active');
+  }
+}
+
+// Set the document title to the value of the page title
+function setPageTitle() {
+  var pageTitle = document.querySelector('#title').textContent;
+  document.title = pageTitle;
+}
+
+// Event listeners
+window.addEventListener('resize', toggleHamburgerMenu);
+window.addEventListener
 
