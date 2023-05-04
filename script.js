bfb8d3d89e32667f471df19153f2a94d5393c6b5
dragon2462 <121273332+dragon2462@users.noreply.github.com>
2023-05-04 10:45:51 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,10 @@
-// Add or remove class to show or hide the hamburger menu based on the viewport width
-function toggleHamburgerMenu() {
-  var hamburger = document.querySelector('.hamburger');
-  var navBar = document.querySelector('.nav-bar');
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.nav-bar');
+const links = document.querySelectorAll('.nav-bar li');
 
-  if (window.innerWidth <= 768) {
-    hamburger.classList.add('active');
-    navBar.classList.add('active');
-  } else {
-    hamburger.classList.remove('active');
-    navBar.classList.remove('active');
-  }
-}
-
-// Set the document title to the value of the page title
-function setPageTitle() {
-  var pageTitle = document.querySelector('#title').textContent;
-  document.title = pageTitle;
-}
-
-// Event listeners
-window.addEventListener('resize', toggleHamburgerMenu);
-window.addEventListener
-
+hamburger.addEventListener('click', () => {
+  navLinks.classList.toggle('open');
+  links.forEach(link => {
+    link.classList.toggle("fade");
+  });
+});
